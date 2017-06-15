5307237e654b04bcfa0c63da573dc8917132e5af
Roderik Muit <rm@wyz.biz>
2017-06-15 21:20:09 +0200

    Change how custom properties are handled in ValueObject constructors; they are injected/remembered in the object. (If other existing custom properties are hardcoded in the class, they are kept. It's expected that people will use either hardcoded or injected properties... but we won't prevent them from using a mix of both.)

--- src/ValueObject.php
+++ src/ValueObject.php
@@ -51,30 +51,44 @@ class ValueObject
     protected $_nullableProperties = [];
 
     /**
-     * All custom property names used by a subclass. (Not required here.)
+     * All custom property names used by a subclass. (Not required to be set.)
      *
      * Array keys are the custom property names and values are the actual
-     * Sharpspring custom field names.
+     * Sharpspring custom field system names.
      *
      * Custom fields can just be set in an object (like e.g.
      *   $lead->custom_id_nr_56dff55bed3f4 = 1;
      * ), but since Sharpspring custom field system names are tedious and may
-     * change when code is used on different Sharpspring accounts /
-     * environments, it may be better to define your own custom property names
-     * in your own subclass of a value object, and map those to the actual
-     * custom field system names. (Defining your own custom property names gets
-     * you IDE autocompletion.)
+     * change when code is used on different Sharpspring accounts/environments,
+     * it may be better to define a subclass of ValueObject that has specific
+     * property names, and and map those properties to the actual custom field
+     * system names. (Defining class properties provides IDE autocompletion.)
      *
-     * The mapping can be handled in different ways, depending on your use case:
-     * define this variable statically in your subclass, or set it in the
-     * constructor, or call setCustomProperties() on a Connection object. (The
-     * latter is more suitable in general code where field names are not always
-     * the same - but then all toArray() calls will have to pass the mapping.)
+     * The mapping can be handled in different ways, depending on the use case:
+     * - define $_customProperties statically in the subclass;
+     * - set it in the constructor;
+     * - or call setCustomProperties() on a Connection object.
+     * The former two ways are more straightforward, but not suitable in general
+     * code that is not tied to specific Sharpspring accounts, because in that
+     * case we don't know beforehand how to derive the field system names. This
+     * means all toArray() calls need to pass the mapping (because
+     * $_customProperties is never set in the class); most create/update methods
+     * on the Connection object do this transparently after
+     * setCustomProperties() is called.
+     *
+     * Beware that all code in this library makes the following implicit
+     * assumptions, which you are expected to follow:
+     * - No two property names are mapped to the sane custom field system name.
+     * - No property name ever doubles as a field system name, or vice versa; in
+     *   other words, the two 'namespaces' of properties and custom field names
+     *   never overlap. This should be easy enough to adhere to, since field
+     *   system names always end with an underscore followed by a 13 digit
+     *   hexadecimal number.
      */
     protected $_customProperties = [];
 
     /**
-     * Constructs an object, converting custom system fields.
+     * Constructs an object, converting custom field system names to properties.
      *
      * @param array $values
      *   Values to initialize in the object. We assume custom field values are
@@ -82,21 +96,27 @@ class ValueObject
      *   property will be set to this value.
      * @param array $custom_properties
      *   The custom property name to Sharpspring field system name mapping,
-     *   which should be used. Any fields not specified here will be taken from
-     *   $this->_customProperties if they are defined there.
+     *   which should be used in this object (and remembered for toArray()
+     *   return values). If the class definition already specifies a mapping,
+     *   any properties not present in this argument will be retained.
      */
     public function __construct(array $values = [], array $custom_properties = [])
     {
-        $custom_properties += $this->_customProperties;
-        // We assume no duplicate properties are set to the same field system
-        // name. If so, it is unclear which property will be filled.
-        $custom_fields = array_flip($custom_properties);
+        // Inject the properties in this object. Keep any unspecified properties
+        // that are in the class definition.
+        $this->_customProperties = $custom_properties + $this->_customProperties;
 
         // Initialize nullable properties first; they can be overwritten by
         // null values if these are provided in $values.
         foreach ($this->_nullableProperties as $name) {
             $this->$name = "\0";
         }
+
+        // Set property values, assuming values are keyed by system name.
+        // (Custom property names will also work, however.) We blindly assume no
+        // duplicate properties are mapped to the same field system name. If so,
+        // it is unclear which property will be set.
+        $custom_fields = array_flip($this->_customProperties);
         foreach ($values as $name => $value) {
             if (isset($custom_fields[$name])) {
                 $name = $custom_fields[$name];
@@ -107,6 +127,10 @@ class ValueObject
 
     /**
      * Converts our object to an array, converting custom properties.
+     *
+     * This is (/ should be) always used for converting our class instances into
+     * 'object representations' (arrays) that can be sent into Sharpspring with
+     * e.g. create/update calls.
      *
      * @param array $custom_properties
      *   The custom property name to Sharpspring field system name mapping,
